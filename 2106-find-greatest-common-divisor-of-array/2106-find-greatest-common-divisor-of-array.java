20efe518ce6b14cf24cbf267f477afb2204f5a1f
Devanayaki <devanayakivdeva@gmail.com>
2025-04-18 07:24:35 +0530

    Time: 3 ms (25.12%), Space: 43.5 MB (31.06%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.java
@@ -4,7 +4,7 @@ class Solution {
         int max=Arrays.stream(nums).max().orElse(-1);
         int min=Arrays.stream(nums).min().orElse(-1);
         int gcd=1;
-        for(int i=1;i<=min && i<=max;i++){
+        for(int i=1;i<=min ;i++){
             if(min%i==0 && max%i==0)
             gcd=i;
         }
