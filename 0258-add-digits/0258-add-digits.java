46d1dea727ff5db645e1c90398d68e4d36a03f0d
Devanayaki <devanayakivdeva@gmail.com>
2025-04-22 09:53:18 +0530

    Time: 1 ms (42.68%), Space: 40.9 MB (55.44%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum = 0;
+        while (num>0 || sum>9) {
+            if (num==0) { 
+                num=sum;
+                sum=0;
+            }
+            int d= num%10; 
+            sum+=d;         
+            num/=10;        
+        }
+        return sum; 
+    }
+}
