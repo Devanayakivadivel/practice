79988113a3750393433a696dda14f397c71598fd
Devanayaki <devanayakivdeva@gmail.com>
2025-07-10 06:45:23 +0530

    Time: 6 ms (15.62%), Space: 41 MB (49.46%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -3,8 +3,8 @@ class Solution {
         String res="";
         while(col_num>0){
             col_num--;
-            int d=col_num%26;
-            char c=(char )('A'+d);
+           // int d=col_num%26;
+            char c=(char )('A'+(col_num%26));
             res=c+res;
             col_num/=26;
 
